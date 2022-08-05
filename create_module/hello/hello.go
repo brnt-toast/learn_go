2ed627a83cad5fb6e4803a79bc6eb9a9905ae514
brnt-toast <yoyo.beerme@gmail.com>
2022-08-05 19:44:26 -0400

    [+] logging

--- create_module/hello/hello.go
+++ create_module/hello/hello.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main(){
-	// get a gretting message and print
-	message := greetings.Hello("Burt")
+	// Set properts of the predefinded looger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 
+	// get a gretting message and print
+	message, err := greetings.Hello("")
+	// If an error was returned, print to the console and exit
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+
+	// If no error was returned, print the returned message to console
 	fmt.Println(message)
 }
