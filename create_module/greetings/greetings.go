e73c1b7ed5c9d5d5d62ae65a8c6eacdd6358f813
brnt-toast <yoyo.beerme@gmail.com>
2022-08-05 19:39:24 -0400

    [+] error handling

--- create_module/greetings/greetings.go
+++ create_module/greetings/greetings.go
@@ -1,11 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name, return an error with a message
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	// Return a greeting that returns the name in varible message
 	message := fmt.Sprintf("Hi, %v. Welcome!", name) // := decleare & initialize using the value on the right
 // %v => value 
-	return message
+	return message, nil
 }
