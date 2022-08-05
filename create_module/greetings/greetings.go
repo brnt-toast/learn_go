792f99363fd38b3971766db038399ec5efbf88dd
brnt-toast <yoyo.beerme@gmail.com>
2022-08-05 19:11:26 -0400

    [+] comments

--- create_module/greetings/greetings.go
+++ create_module/greetings/greetings.go
@@ -5,7 +5,7 @@ import "fmt"
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
 	// Return a greeting that returns the name in varible message
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-
+	message := fmt.Sprintf("Hi, %v. Welcome!", name) // := decleare & initialize using the value on the right
+// %v => value 
 	return message
 }
